b6513983c7688c6cc579cd212e4e972c69785002
Lee-Kyung-Sup(이경섭) <120997897+Lee-Kyung-Sup@users.noreply.github.com>
2024-01-29 11:51:47 +0900

    Function-jump_character
    leekyungsup

--- Assets/Scripts/lks/Controllers/CharacterController.cs
+++ Assets/Scripts/lks/Controllers/CharacterController.cs
@@ -6,16 +6,52 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpSpeed;
     Rigidbody2D rb;
-
+    SpriteRenderer spriteRenderer;
+    Animator animator;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
-    private void FixedUpdate()
+    void Update()
     {
-        //Move
+        //jump
+        if (Input.GetButtonUp("Jump") && !animator.GetBool("isJump"))
+        {
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            animator.SetBool("isJump", true);
+        }
+
+            //friction force
+            if (Input.GetButtonUp("Horizontal"))
+        {
+            rb.velocity = new Vector2(0.5f * rb.velocity.normalized.x, rb.velocity.y);
+        }
+
+        //change direction
+        if(Input.GetButtonDown("Horizontal"))
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+
+        //state change
+        if(Mathf.Abs(rb.velocity.normalized.x) < 0.5)
+        {
+            animator.SetBool("isRun", false);
+        }
+        else
+        {
+            animator.SetBool("isRun", true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //move
         float x = Input.GetAxisRaw("Horizontal");
 
         rb.AddForce(Vector2.right * x, ForceMode2D.Impulse);
@@ -27,6 +63,21 @@ public class CharacterController : MonoBehaviour
         else if(rb.velocity.x < maxSpeed*(-1)) //left
         {
             rb.velocity = new Vector2(maxSpeed*(-1), rb.velocity.y);
+        }
+
+        //jump landing
+        if(rb.velocity.y < 0)
+        {
+            Debug.DrawRay(rb.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rb.position, Vector3.down, 1, LayerMask.GetMask("platform"));
+
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                {
+                    animator.SetBool("isJump", false);
+                }
+            }
         }
     }
 }
