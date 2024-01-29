dd1e7f3b7fef478bca1d7b8e8ebf6ff8b453143e
dch1114 <dch1114@naver.com>
2024-01-29 12:00:20 +0900

    function_젤리 떨어트리기

---
+++ Assets/Scripts/SDG/RandomSpawn.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> jellyPrefabs;
+
+    void Start()
+    {
+        StartCoroutine(CreateJellyRoutine());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator CreateJellyRoutine()
+    {
+        while (true)
+        {
+            CreateRandomJelly();
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    private void CreateRandomJelly()
+    {
+        if (jellyPrefabs.Count == 0)
+        {
+            Debug.LogError("jellyPrefabs");
+            return;
+        }
+
+        GameObject selectedJellyPrefab = jellyPrefabs[Random.Range(0, jellyPrefabs.Count)];
+
+        Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(Random.Range(0.0f, 1.0f), 1.1f, 0));
+        pos.z = 0.0f;
+
+        Instantiate(selectedJellyPrefab, pos, Quaternion.identity);
+    }
+}
