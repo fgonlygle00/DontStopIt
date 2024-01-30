63a3a0588ee0b21800958c709e7ebe7e0fc60b0b
dudtjs126 <dudtjs126@gmail.com>
2024-01-30 09:57:00 +0900

    [Fix] 코드 주석 처리

--- Assets/Scripts/BlockManager.cs
+++ Assets/Scripts/BlockManager.cs
@@ -7,19 +7,14 @@ public class BlockManager : MonoBehaviour
     public GameObject block;
     public Grid grid;
 
-    public int numberOfPlatforms = 5;
-    public int horizontalSpacing = 2;
-    public int verticalSpacing = 2;
-
-    // Start is called before the first frame update
     void Start()
     {
-        int length = 13;
-        for (int i = 0; i < length; i++)
+        int length = 13;   // 블록 쌓은 층 수를 의미
+        for (int i = 0; i < length; i++) // 블록 만드는 for 문
         {
-            int tileCount = GetTileCount();
-            int[] positionX = GetRandomGridPosition(tileCount);
-            for (int j = 0; j < tileCount; j++)
+            int tileCount = GetTileCount(); // 타일 개수 랜덤 값
+            int[] positionX = GetRandomGridPosition(tileCount); // 타일 생성 좌표
+            for (int j = 0; j < tileCount; j++) // 타일 개수만큼 좌표에 타일 생성
             {
                 Vector3 randomPos = new Vector3(-3.5f + positionX[j] * 0.5f, 6.5f + i * 2, 0);
                 Instantiate(block, randomPos, Quaternion.identity);
@@ -27,6 +22,7 @@ public class BlockManager : MonoBehaviour
         }
     }
 
+    // 타일 생성 좌표
     private int[] GetRandomGridPosition(int tileCount)
     {
         List<int> availableNumbers = new List<int>();
@@ -35,15 +31,16 @@ public class BlockManager : MonoBehaviour
             availableNumbers.Add(i);
         }
 
-        int[] positionX = new int[tileCount];
+        int[] positionX = new int[tileCount]; // 랜덤 좌표 결과 담을 배열
         for (int i = 0; i < tileCount; i++)
         {
-            positionX[i] = PickNumber(availableNumbers);
+            positionX[i] = PickNumber(availableNumbers); // 블록이 생성된 좌표가 겹치는지 확인
         }
 
         return positionX;
     }
 
+    // 블록이 생성된 좌표가 겹치는지 확인
     private int PickNumber(List<int> availableNumbers)
     {
         int pickedNumber;
@@ -51,18 +48,19 @@ public class BlockManager : MonoBehaviour
 
         do
         {
-            int index = Random.Range(0, availableNumbers.Count);
+            int index = Random.Range(0, availableNumbers.Count); // 숫자를 랜덤으로 1개 선택
             pickedNumber = availableNumbers[index];
-            isValidNumber = CheckIfValidNumber(pickedNumber, availableNumbers);
+            isValidNumber = CheckIfValidNumber(pickedNumber, availableNumbers); // 뽑은 숫자가 안겹치는지 확인
 
         } while (!isValidNumber);
 
-        RemoveNumbersInRange(pickedNumber, availableNumbers);
+        RemoveNumbersInRange(pickedNumber, availableNumbers); // 뽑은 숫자를 겹치지 않도록 배열에서 값 제거
         return pickedNumber;
     }
+
     private bool CheckIfValidNumber(int number, List<int> availableNumbers)
     {
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 4; i++) // 뽑은 숫자를 기준으로 블록을 겹치지 않게 깔 수 있는지 확인
         {
             int numToCheck = (number + i) % 19;
             if (!availableNumbers.Contains(numToCheck))
@@ -75,7 +73,7 @@ public class BlockManager : MonoBehaviour
 
     private void RemoveNumbersInRange(int number, List<int> availableNumbers)
     {
-        for (int i = -1; i < 5; i++)
+        for (int i = -1; i < 5; i++) // 뽑은 숫자 기준으로 6칸을 제거 (블록 4칸 + 좌우 2칸)
         {
             int numToRemove = (number + i) % 19;
             availableNumbers.Remove(numToRemove);
@@ -87,7 +85,6 @@ public class BlockManager : MonoBehaviour
         return Random.Range(2, 4);
     }
 
-    // Update is called once per frame
     void Update()
     {
     }    
